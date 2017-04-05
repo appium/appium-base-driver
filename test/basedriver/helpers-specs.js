c43f0cedf059cd9f0f57d761c18e624ea2edce58
dan@saucelabs.com <dan@saucelabs.com>
2017-04-05 12:53:45 -0700

    Added merge capabilities

--- test/basedriver/helpers-specs.js
+++ test/basedriver/helpers-specs.js
@@ -1,8 +1,8 @@
-import { isPackageOrBundle, parseCapabilities, validateCapabilities } from '../../lib/basedriver/helpers';
+import { isPackageOrBundle, parseCapabilities, validateCapabilities, mergeCapabilities, matchCapabilities, processCapabilities } from '../../lib/basedriver/helpers';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 
-//const should = chai.should();
+const should = chai.should();
 chai.use(chaiAsPromised);
 
 describe('helpers', () => {
@@ -20,12 +20,9 @@ describe('helpers', () => {
   // Tests based on: https://www.w3.org/TR/webdriver/#dfn-validate-capabilities
   describe('#validateCapabilities', () => {
     it('returns invalid argument error if "capability" is not a JSON object (1)', () => {
-      (function () { validateCapabilities(); }).should.throw(/must be a JSON object/);
-      (function () { validateCapabilities(undefined); }).should.throw(/must be a JSON object/);
-      (function () { validateCapabilities(null); }).should.throw(/must be a JSON object/);
-      (function () { validateCapabilities(1); }).should.throw(/must be a JSON object/);
-      (function () { validateCapabilities(true); }).should.throw(/must be a JSON object/);
-      (function () { validateCapabilities('string'); }).should.throw(/must be a JSON object/);
+      for (let arg of [undefined, null, 1, true, 'string']) { 
+        (function () { validateCapabilities(arg); }).should.throw(/must be a JSON object/);
+      }
     });
 
     it('returns result {} by default if caps is empty object and no constraints provided (2)', () => {
@@ -77,6 +74,40 @@ describe('helpers', () => {
     });
   });
 
+  // Tests based on: https://www.w3.org/TR/webdriver/#dfn-merging-capabilities
+  describe('#mergeCapabilities', () => {
+    it('returns a result that is {} by default (1)', () => {
+      mergeCapabilities().should.deep.equal({});
+    });
+
+    it('returns a result that matches primary by default (2, 3)', () => {
+      mergeCapabilities({hello: 'world'}).should.deep.equal({hello: 'world'});
+    });
+
+    it('returns invalid argument error if primary and secondary have matching properties (4)', () => {
+      (() => mergeCapabilities({hello: 'world'}, {hello: 'whirl'})).should.throw(/property hello should not exist on both primary and secondary/);
+    });
+
+    it('returns a result with keys from primary and secondary together', () => {
+      let primary = {
+        a: 'a',
+        b: 'b',
+      };
+      let secondary = {
+        c: 'c',
+        d: 'd',
+      };
+      mergeCapabilities(primary, secondary).should.deep.equal({
+        a: 'a', b: 'b', c: 'c', d: 'd',
+      });
+    });
+  });
+
+  // Tests based on: https://www.w3.org/TR/webdriver/#dfn-matching-capabilities
+  describe('#matchCapabilities', () => {
+    // TODO: Do we need this?
+  });
+
   // Tests based on: https://www.w3.org/TR/webdriver/#processing-capabilities
   describe('#parseCapabilities', () => {
     it('sets "capabilitiesRequest" to property named "capabilities" (1)', () => {
@@ -85,12 +116,9 @@ describe('helpers', () => {
     });
 
     it('returns invalid argument error if "capabilities" is not an object (1.1)', () => {
-      (function (){ parseCapabilities(); }).should.throw(/must be a JSON object/);
-      (function (){ parseCapabilities(undefined); }).should.throw(/must be a JSON object/);
-      (function (){ parseCapabilities(null); }).should.throw(/must be a JSON object/);
-      (function (){ parseCapabilities(1); }).should.throw(/must be a JSON object/);
-      (function (){ parseCapabilities(true); }).should.throw(/must be a JSON object/);
-      (function (){ parseCapabilities('string'); }).should.throw(/must be a JSON object/);
+      for (let arg of [undefined, null, 1, true, 'string']) {
+        (function (){ parseCapabilities(arg); }).should.throw(/must be a JSON object/);
+      }
     });
 
     it('sets "requiredCapabilities" to property named "alwaysMatch" (2)', () => {
@@ -114,19 +142,17 @@ describe('helpers', () => {
       parseCapabilities({}, {}).allFirstMatchCapabilities.should.deep.equal([]);
     });
 
-    it('returns invalid argument error if "firstMatch" is not an array (3.2)', () => {
-      (function (){ parseCapabilities({}, {}, 'hello'); }).should.throw(/must be a JSON array or undefined/);
-      (function (){ parseCapabilities({}, {}, 1); }).should.throw(/must be a JSON array or undefined/);
-      (function (){ parseCapabilities({}, {}, {}); }).should.throw(/must be a JSON array or undefined/);
-      (function (){ parseCapabilities({}, {}, true); }).should.throw(/must be a JSON array or undefined/);
-      (function (){ parseCapabilities({}, {}, false); }).should.throw(/must be a JSON array or undefined/);
+    it('returns invalid argument error if "firstMatch" is not an array and is not undefined (3.2)', () => {
+      for (let arg of [null, 1, true, 'string']) {
+        (function (){ parseCapabilities({}, {}, arg); }).should.throw(/must be a JSON array or undefined/);
+      }
     });
 
     it('has "validatedFirstMatchCapabilities" property that is [] by default (4)', () => {
       parseCapabilities({}).validatedFirstMatchCapabilities.should.deep.equal([]);
     });
 
-    describe.only('returns a "validatedFirstMatchCapabilities" array (5)', () => {
+    describe('returns a "validatedFirstMatchCapabilities" array (5)', () => {
       it('that equals "firstMatch" if firstMatch is one empty object and there are no constraints', () => {
         parseCapabilities({}, {}, [{}]).validatedFirstMatchCapabilities.should.deep.equal([{}]);
       });
@@ -161,6 +187,30 @@ describe('helpers', () => {
         let firstMatch = [{foo: 'bar'}, 'foo'];
         (() => parseCapabilities({}, {}, firstMatch, {})).should.throw(/must be a JSON object/);
       });
+    });
+
+    describe('returns a matchedCapabilities object (6)', () => {
+      it('which is undefined by default', () => {
+        should.not.exist(parseCapabilities({}).matchedCapabilities);
+      });
+
+      it('merges capabilities together', () => {
+        parseCapabilities({}, {hello: 'world'}, [{foo: 'bar'}]).matchedCapabilities.should.deep.equal({hello: 'world', foo: 'bar'});
+      });
+
+      it('with merged capabilities', () => {
+        parseCapabilities({}, {hello: 'world'}, [{hello: 'bar', foo: 'foo'}, {foo: 'bar'}]).matchedCapabilities.should.deep.equal({hello: 'world', foo: 'bar'});
+      });
+    });
+  });
+
+  describe('#processCapabilities', () => {
+    it('should return null by default', () => {
+      should.not.exist(processCapabilities({}));
+    });
+
+    it('should return merged capabilities', () => {
+      processCapabilities({}, {hello: 'world'}, [{foo: 'bar'}]).should.deep.equal({hello: 'world', foo: 'bar'});
     });
   });
 });
