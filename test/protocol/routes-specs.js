fa75c0d4ba4e43910693e62f02996c6314772c21
Mykola Mokhnach <nick+1@wire.com>
2018-03-24 19:29:55 +0100

    Fix get endpoint

--- test/protocol/routes-specs.js
+++ test/protocol/routes-specs.js
@@ -40,7 +40,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('c61cda40');
+      hash.should.equal('afd1f1e1');
     });
   });
 
