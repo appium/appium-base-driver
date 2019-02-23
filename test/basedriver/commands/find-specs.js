abbefd8cd388a30bf8178cf07d6407297aa7146a
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-02-24 00:56:03 +0900

    add fixScaleTemplateImage as a settings

--- test/basedriver/commands/find-specs.js
+++ test/basedriver/commands/find-specs.js
@@ -93,6 +93,27 @@ describe('finding elements by image', function () {
       imgEl.template.should.eql(newTemplate);
       compareStub.args[0][2].should.eql(newTemplate);
     });
+
+    it('should fix template size scale if requested', async function () {
+      const d = new TestDriver();
+      const newTemplate = 'iVBORbaz';
+      const {compareStub} = basicStub(d);
+      await d.settings.update({fixScaleTemplateImage: true});
+      sinon.stub(d, 'fixScaleTemplateImage').returns(newTemplate);
+      const imgElProto = await d.findByImage(template, {multiple: false});
+      const imgEl = basicImgElVerify(imgElProto, d);
+      imgEl.template.should.eql(newTemplate);
+      compareStub.args[0][2].should.eql(newTemplate);
+    });
+    it('should not fix template size scale if it is not requested', async function () {
+      const d = new TestDriver();
+      const newTemplate = 'iVBORbaz';
+      basicStub(d);
+      await d.settings.update({});
+      sinon.stub(d, 'fixScaleTemplateImage').returns(newTemplate);
+      d.fixScaleTemplateImage.callCount.should.eql(0);
+    });
+
     it('should throw an error if template match fails', async function () {
       const d = new TestDriver();
       const {compareStub} = basicStub(d);
