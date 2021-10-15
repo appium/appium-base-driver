12ecb473d73cbc9a39bf9dfa3093177f1e641b13
Mykola Mokhnach <mokhnach@gmail.com>
2021-10-15 18:56:25 +0200

    chore: Add params to `setWindowRect` handler (#529)

--- test/protocol/routes-specs.js
+++ test/protocol/routes-specs.js
@@ -40,7 +40,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('5d9aaa31');
+      hash.should.equal('5052d81b');
     });
   });
 
